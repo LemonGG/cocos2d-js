01b35482d3f74e1b6f5fbb25323611b18911c7b3
LemonGG <Leo@leo.local>
2015-08-14 14:59:11 +0800

    分享
    添加了分享到微信代码

---
+++ leocpt/Share.js
@@ -0,0 +1,19 @@
+
+
+
+shareToWeiXin:function()
+{
+
+	// 只有在微信浏览器中可以使用
+	if(!WeixinJSBridge)return;
+	WeixinJSBridge.invoke('shareTimeline',{
+                "img_url": 'http://tv.59600.com/Idol/logo/75.png',
+                "img_width": "75",
+                "img_height": "75",
+                "link": "http://tv.59600.com/Idol/",
+                "desc": "《偶像来了》女神排行榜。玩游戏，支持你喜欢的偶像！",
+                "title": "《偶像来了》女神排行榜"
+            }, function(res) {
+                //_report('timeline', res.err_msg);
+            });
+};
