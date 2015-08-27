cc8560c9d9bb46cc1e20bc1fc28bbb103dc1ce69
LemonGG <Leo@leo.local>
2015-08-27 11:25:25 +0800

    chipmunk
    创建物理世界 点击生成box刚体

---
+++ chipmunk/test_chipmunk/src/resource.js
@@ -0,0 +1,11 @@
+var res = {
+   head_0:"res/head_0.jpg",
+   head_1:"res/head_1.jpg",
+   head_2:"res/head_2.jpg",
+   head_3:"res/head_3.jpg"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
